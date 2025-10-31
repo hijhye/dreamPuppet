ee8eebd9bbadf9e1c181bd2d291ad6fed8061fa5
collollll <hck2061@naver.com>
2025-10-31 17:56:16 +0900

    251031

--- about/about.js
+++ about/about.js
@@ -1,36 +1,9 @@
 $(function () {
-  // .con01
-  // $(".con01 .values div").on("mouseenter", function () {
-  //   let i = $(this).index();
-  //   $(this).stop().animate({ "font-size": "20px" });
-  //   $(".con01 li").removeClass("up");
-  //   $(".con01 li").eq(i).addClass("up");
-  //   // $(".con01 ul li")
-  //   //   .eq(i)
-  //   //   .css({ display: "block" })
-  //   //   .animate({ opacity: 1 }, 700, {
-  //   //     duration: 500,
-  //   //     step: function () {
-  //   //       $(this).find("h3").css("transform", "rotateX(360deg)");
-  //   //     },
-  //   //   });
-  // });
-
-  // $(".con01 .values div ").on("mouseleave", function () {
-  //   let i = $(this).index();
-  //   $(this).stop().animate({ "font-size": "16px" });
-  // });
-
-  //   .con02
+  // .con02
   $(".con02 .menu li").on("mouseenter", function () {
     let i = $(this).index();
-
-    // $(".con02 .bg li").removeClass("look");
-    // $(".con02 .bg li").eq(i).addClass("look");
     $(".con02 .title li").removeClass("look");
     $(".con02 .title li").eq(i).addClass("look");
-    //   $(".con02 .menu li").removeClass("look");
-    //   $(this).addClass("look");
   });
 
   // .con03
@@ -39,7 +12,6 @@ $(function () {
       speed: 1.5,
       pauseOnHover: true,
       pauseOnTouch: false,
-      //   direction: "backwards"
     });
   });
 });
@@ -68,10 +40,6 @@ gsap
   );
 
 // con01
-// gsap
-//   .timeline()
-//   .to(".con01 li h3", { display: "block" }, 0)
-//   .fromTo(".con01 li h3", { rotateX: "270deg" }, { rotateX: "360deg" }, 0.2);
 let values = document.querySelectorAll(".con01 .values div");
 let text = document.querySelectorAll(".con01 ul li");
 values.forEach((i, index) => {
@@ -80,8 +48,6 @@ values.forEach((i, index) => {
       gsap.to(i, { opacity: 0, duration: 1.5 });
       i.classList.remove("up");
     });
-    // gsap.timeline().to(text[index], { opacity: 1 }, 0);
-    // text[index].style.opacity = 1;
     gsap.to(text[index], { opacity: 1, duration: 1.5 });
     text[index].classList.add("up");
 
@@ -121,9 +87,7 @@ menu.forEach((i, index) => {
     bg.forEach((i) => {
       gsap.killTweensOf(i);
       i.style.display = "none";
-      //   i.classList.remove("look");
     });
-    // bg[index].classList.add("look");
 
     gsap.fromTo(
       bg[index],
@@ -131,13 +95,7 @@ menu.forEach((i, index) => {
       { opacity: 1, duration: 1.3, display: "block" }
     );
   });
-  // i.addEventListener("mouseout", function () {
-  //   bg.forEach((i) => {
-  //     i.style.display = "none";
-  //   });
-  // });
 });
-// 작동이 되긴함! 근데 마우스 뗐을 때 원래대로 안돌아가서, 여러번 마우스 올리면 결국 li 전체가 다 나와버리게 됨
 
 // 플로팅
 let float = document.querySelector("#floating .floatFix");
@@ -147,8 +105,6 @@ let close = document.querySelector(".inquery i");
 
 float.addEventListener("mouseenter", () => {
   gsap.timeline().to("#floating .top", { top: "-35%" });
-  // .to("#floating .top", { display: "inline-block" }, 0)
-  // .fromTo("#floating .top", { y: 100 }, { y: 0 }, 0.1);
 });
 
 float.addEventListener("mouseleave", (e) => {
@@ -157,7 +113,6 @@ float.addEventListener("mouseleave", (e) => {
     return;
   }
   gsap.timeline().to("#floating .top", { top: "50%" });
-  // .to("#floating .top", { display: "" }, 0.1);
 });
 goTop.addEventListener("mouseleave", (e) => {
   check = e.relatedTarget;
@@ -169,7 +124,6 @@ goTop.addEventListener("mouseleave", (e) => {
 
 float.addEventListener("click", () => {
   gsap.timeline().to(inquery, { top: "-500px", display: "block", opacity: 1 });
-  // .fromTo("#floating .inquery", { display: "block", y: 100 }, { y: 0 }, 0.1);
 });
 close.addEventListener("click", () => {
   gsap.timeline().to(inquery, { top: 0, display: "none", opacity: 0 });
@@ -177,28 +131,3 @@ close.addEventListener("click", () => {
 $(goTop).on("click", function () {
   $("html, body").animate({ scrollTop: 0 }, 300);
 });
-
-// con01
-// let values = document.querySelectorAll(".con01 .values div");
-// let text = document.querySelectorAll(".con01 ul li");
-// values.forEach((i, index) => {
-//   i.addEventListener("mouseover", function () {
-//     text[index].classList.add("look");
-
-//     gsap
-//       .timeline()
-//       .to(text[index], { display: "block" }, 0)
-//       .to(text[index], { opacity: 1, rotateX: "360deg" }, 0.3);
-
-//     bg.forEach((i) => {
-//       gsap.killTweensOf(i);
-//       i.style.display = "none";
-//     });
-
-//     gsap.fromTo(
-//       bg[index],
-//       { opacity: 0 },
-//       { opacity: 1, duration: 1.3, display: "block" }
-//     );
-//   });
-// });
